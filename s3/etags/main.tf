331058e8b7a45b9ca1901ff41ceaf05b67797882
Sharon Mae <91280927+SharonMae@users.noreply.github.com>
2024-10-30 23:16:25 +0000

    refactor bash scripts, and provide etag

--- s3/etags/main.tf
+++ s3/etags/main.tf
@@ -7,11 +7,18 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "og-default-bucket" {
+provider "aws" {
+  region = "us-west-2"
 }
 
-resource "aws_s3_bucket_object" "object" {
-  bucket = resource.aws_s3_bucket.og-default-bucket
+resource "aws_s3_bucket" "default" {
+  # Specify bucket properties here as needed
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.default.id  # Reference the bucket resource here
   key    = "myfile.txt"
   source = "myfile.txt"
+
+  etag = filemd5("myfile.txt")
 }
