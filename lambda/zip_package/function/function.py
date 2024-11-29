608018197b5caf092ca83e185631d1559705f578
Sharon Mae <duransharonmae@gmail.com>
2024-11-29 01:34:20 +0000

    update zip_package lambda files

---
+++ lambda/zip_package/function/function.py
@@ -0,0 +1,20 @@
+import json
+
+def handler(event, context):
+    # Generate a personalized message using input from the event
+    message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])
+    info = {
+        "Type": "Zip Package",
+        "Version": 1,
+    }
+
+    # Convert into a JSON string
+    info_json = json.dumps(info)
+
+    # Print the JSON string
+    print(info_json)
+
+    # Return the message as a response
+    return {
+        'message': message
+    }
