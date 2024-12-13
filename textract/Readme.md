ec483156975fa612a6ae33a8be1c6b3cf81006fd
Sharon Mae <duransharonmae@gmail.com>
2024-12-13 21:40:11 +0000

    uodate textract files

---
+++ textract/Readme.md
@@ -0,0 +1,2 @@
+aws s3 mb s3://textract-exp-4565 --region us-east-1
+aws s3 tax-doc.png s3://textract-exp-4565
--- textract/main.rb
+++ textract/main.rb
@@ -1,38 +1,17 @@
 require 'aws-sdk-textract'
 
-client = Aws::Textract::Client.new({
+client = Aws::Textract::Client.new
+bucket = 'textract-exp-4565'
+name = 'tax-doc.png'
+resp = client.analyze_document({
   document: { # required
     bytes: "data",
     s3_object: {
-      bucket: "S3Bucket",
-      name: "S3ObjectName",
-      version: "S3ObjectVersion",
+      bucket: bucket,
+      name: name#
+      # version: "S3ObjectVersion",
     },
   },
-  feature_types: ["TABLES"], # required, accepts TABLES, FORMS, QUERIES, SIGNATURES, LAYOUT
-  human_loop_config: {
-    human_loop_name: "HumanLoopName", # required
-    flow_definition_arn: "FlowDefinitionArn", # required
-    data_attributes: {
-      content_classifiers: ["FreeOfPersonallyIdentifiableInformation"], # accepts FreeOfPersonallyIdentifiableInformation, FreeOfAdultContent
-    },
-  },
-  queries_config: {
-    queries: [ # required
-      {
-        text: "QueryInput", # required
-        alias: "QueryInput",
-        pages: ["QueryPage"],
-      },
-    ],
-  },
-  adapters_config: {
-    adapters: [ # required
-      {
-        adapter_id: "AdapterId", # required
-        pages: ["AdapterPage"],
-        version: "AdapterVersion", # required
-      },
-    ],
-  }
+  # required, accepts TABLES, FORMS, QUERIES, SIGNATURES, LAYOUT
+  feature_types: ["TABLES"], 
 })
